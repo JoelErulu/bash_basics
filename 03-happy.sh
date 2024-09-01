f98a4032963a423f7288cb0c4a0dbf8c09eb41ed
JOELERULU <joelerulu088@gmail.com>
2024-09-01 10:05:15 -0400

    Parallel file

--- 03-happy.sh
+++ 03-happy.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-echo "You are happy?"
+echo "Is today the not weekend? (yes/no)"
 read answer
 
 if [ "$answer" = "yes" ]; then
-   echo "Smile :)"
+   echo "You are correct :)"
 else
-   echo "Still Smile :)"
+   echo "You are not correct :("
 fi
+
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
